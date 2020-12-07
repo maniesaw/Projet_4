080172891aee6a7b42edaf1a2a52062404e7e96a
MANIESAW <49059534+maniesaw@users.noreply.github.com>
2020-12-07 10:58:37 +0100

    Ajout du code pour ouvrir les données

--- Projet_code.R
+++ Projet_code.R
@@ -65,4 +65,17 @@ library(IntNMF)
 
 # Mise en place des donnees
 
+# Localisation Data
+setwd(dir = "/Data")
+# A ajuster selon les ordis (localisation des donnees)
+
+GCM1Car = read.csv("gene_count_matrix_1pass_CarcinoidSamples.csv", row.names = "gene_id")
+GCM1Lnen = read.csv("gene_count_matrix_1pass_LnenSamples.csv", row.names = "gene_id")
+
+GFPKMCar = read.csv("gene_FPKM_matrix_CarcinoidSamples.csv", row.names = "gene_id")
+GFPKMLnen = read.csv("gene_FPKM_matrix_LnenSamples.csv", row.names = "gene_id")
+
+NFB = read.csv("NormalisedFilteredBetaTable_CarcinoidSamples.csv", row.names = "X")
+NFM = read.csv("NormalisedFilteredMTable_noInf_CarcinoidSamples.csv", row.names = "X")
+
 # Analyse
